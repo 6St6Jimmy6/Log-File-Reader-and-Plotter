c80041b284e83fab469227de76cc5362f189363d
Jim Hämäläinen <zimbo93@msn.com>
2024-09-16 20:28:00 +0300

    Add project files.

---
+++ Plotter1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace Log_File_Reader_and_Plotter
+{
+    partial class Plotter1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.loadCSVButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.plotToChart1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 286);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 247;
+            this.label3.Text = "Y Axis Right";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 246);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 246;
+            this.label2.Text = "Y Axis Left";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 245;
+            this.label1.Text = "X Axis";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(12, 302);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 244;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 262);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 243;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 222);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 242;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(12, 358);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 241;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Visible = false;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 329);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 240;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Visible = false;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(12, 12);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(60, 25);
+            this.closeButton.TabIndex = 238;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click_1);
+            // 
+            // loadCSVButton
+            // 
+            this.loadCSVButton.Location = new System.Drawing.Point(12, 43);
+            this.loadCSVButton.Name = "loadCSVButton";
+            this.loadCSVButton.Size = new System.Drawing.Size(75, 51);
+            this.loadCSVButton.TabIndex = 236;
+            this.loadCSVButton.Text = "Load log file";
+            this.loadCSVButton.UseVisualStyleBackColor = true;
+            this.loadCSVButton.Click += new System.EventHandler(this.loadCSVButton_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(485, 34);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(429, 261);
+            this.textBox1.TabIndex = 237;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(486, 301);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(428, 57);
+            this.textBox2.TabIndex = 239;
+            this.textBox2.Visible = false;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(139, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1080, 720);
+            this.chart1.TabIndex = 248;
+            this.chart1.Text = "chart1";
+            // 
+            // plotToChart1
+            // 
+            this.plotToChart1.Location = new System.Drawing.Point(12, 100);
+            this.plotToChart1.Name = "plotToChart1";
+            this.plotToChart1.Size = new System.Drawing.Size(75, 51);
+            this.plotToChart1.TabIndex = 249;
+            this.plotToChart1.Text = "Plot to chart";
+            this.plotToChart1.UseVisualStyleBackColor = true;
+            this.plotToChart1.Click += new System.EventHandler(this.plotToChart1_Click);
+            // 
+            // Plotter1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1258, 778);
+            this.Controls.Add(this.plotToChart1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.loadCSVButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Plotter1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Plotter1_FormClosing);
+            this.Load += new System.EventHandler(this.Plotter1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button loadCSVButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button plotToChart1;
+    }
+}
+
---
+++ PointChart.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Log_File_Reader_and_Plotter
+{
+/*
+Thanks for EETechStuff this awesome video that shows how to build a Chart class.
+https://www.youtube.com/watch?v=YH5psj_dN1Q
+ */
+    class PointChart
+    {
+        #region Docs
+
+        /* This Class defines a Line Chart which has the following features:
+         *  - Ability to define Prmary & Secondary (Y1 & Y2) Axes
+         *  - Ability to do Manual or Auto scaling to Y1 & Y2 Axes
+         *  - Ability to instantate one or more instances of this class to configure multiple charts 
+         *  
+         *  Secondary (Y2) Axis:
+         *  The "Y2Axis()" method adds a new Series to the chart and configures the Y2 Axis. When configuring Secondary (Y2) axis gridlines, 
+         *  it uses the existing Major & Minor grid lines of the Primary (Y1) axis, and doesn't draw Y2 gridlines. 
+         *  It calculates a Y2 interval that corresponds to the Y1 gridlines.
+         *  
+         *  At any time the user can call one of the Class methods "AutoScaleY()" or "AutoScaleY2()" to do either manual or auto scaling of either Y1 or Y2 axes.
+         */
+
+        #endregion
+
+        #region Fields
+
+        /*
+         * These fields are used to calculate Secondary (Y2) Axis intervals to ensure they match the Primary (Y1) Axis intervals, since no Y2 axis gridlines are drawn.
+        */
+        double YAxisMin;
+        double YAxisMax;
+        double YAxisMajorInt;
+
+        /*
+         * Define the Font used for all axes
+         */
+        //Font AxisFont = new Font("Microsoft Sans Serif", 8f, FontStyle.Bold);
+
+        #endregion
+
+        #region Constructor
+
+        public PointChart(Chart chartName)
+        {
+            chartName.Series["Series1"].ChartType = SeriesChartType.Point;
+
+
+
+            chartName.ChartAreas["ChartArea1"].BorderColor = Color.Empty;
+            chartName.ChartAreas["ChartArea1"].BorderDashStyle = ChartDashStyle.Solid;
+            chartName.ChartAreas["ChartArea1"].BorderWidth = 1;
+
+            chartName.ChartAreas["ChartArea1"].BackColor = System.Drawing.Color.White;
+            chartName.ChartAreas["ChartArea1"].BackSecondaryColor = Color.Empty;
+
+            chartName.ChartAreas["ChartArea1"].BackGradientStyle = GradientStyle.None;
+            chartName.ChartAreas["ChartArea1"].BackHatchStyle = ChartHatchStyle.None;
+
+            chartName.ChartAreas["ChartArea1"].BackImage = "";
+            chartName.ChartAreas["ChartArea1"].BackImageAlignment = ChartImageAlignmentStyle.TopLeft;
+            chartName.ChartAreas["ChartArea1"].BackImageTransparentColor = Color.Empty;
+            chartName.ChartAreas["ChartArea1"].BackImageWrapMode = ChartImageWrapMode.Tile;
+
+            chartName.ChartAreas["ChartArea1"].ShadowColor = Color.Black;
+            chartName.ChartAreas["ChartArea1"].ShadowOffset = 0;
+
+            //chart.Legends.Clear();
+        }
+
+        #endregion
+
+        #region Methods
+        public void XAxis(Chart chartName, string labelMajorFamilyName, float labelMajorEmSizef, FontStyle labelMajorStyle, Color labelTextColor, int labelMajorDecimals, double min, double max, double majorInterval, bool minorEnabled, ChartDashStyle minorDashStyle, double minorIntFrac) // ChartName, sMajorLabelFontName, MajorLabelFontSizef, MajorLabelFont.Style, MinValue, MaxValue, MajorGridInterval, MinorGridEnabled, MinorGridIntervalFraction
+        {
+
+
+            chartName.ChartAreas["ChartArea1"].AxisX.LabelStyle.Font = new Font(labelMajorFamilyName, labelMajorEmSizef, labelMajorStyle);
+            chartName.ChartAreas["ChartArea1"].AxisX.LabelStyle.ForeColor = labelTextColor;
+            chartName.ChartAreas["ChartArea1"].AxisX.LabelStyle.Format = "F" + Convert.ToString(labelMajorDecimals);
+
+            chartName.ChartAreas["ChartArea1"].AxisX.IsLabelAutoFit = false;
+            chartName.ChartAreas["ChartArea1"].AxisX.LabelAutoFitMaxFontSize = 20;
+
+            chartName.ChartAreas["ChartArea1"].AxisX.Crossing = double.NaN;
+            chartName.ChartAreas["ChartArea1"].AxisX.Enabled = AxisEnabled.Auto;
+
+            chartName.ChartAreas["ChartArea1"].AxisX.IsInterlaced = false;
+            chartName.ChartAreas["ChartArea1"].AxisX.InterlacedColor = Color.Black;
+
+            chartName.ChartAreas["ChartArea1"].AxisX.IsLogarithmic = false;
+            chartName.ChartAreas["ChartArea1"].AxisX.LogarithmBase = 10d;
+
+
+
+            chartName.ChartAreas["ChartArea1"].AxisX.Minimum = min;
+            chartName.ChartAreas["ChartArea1"].AxisX.Maximum = max;
+            chartName.ChartAreas["ChartArea1"].AxisX.Interval = majorInterval;
+
+            chartName.ChartAreas["ChartArea1"].AxisX.MinorGrid.Enabled = minorEnabled;
+            chartName.ChartAreas["ChartArea1"].AxisX.MinorGrid.LineDashStyle = minorDashStyle;
+            chartName.ChartAreas["ChartArea1"].AxisX.MinorGrid.LineColor = Color.DarkGray;
+            chartName.ChartAreas["ChartArea1"].AxisX.MinorGrid.LineWidth = 1;
+            chartName.ChartAreas["ChartArea1"].AxisX.MinorGrid.Interval = majorInterval / minorIntFrac;
+            //chartName.ChartAreas["ChartArea1"].AxisX.MinorGrid.IntervalType = DateTimeIntervalType.Number;
+            //chartName.ChartAreas["ChartArea1"].AxisX.MinorGrid.IntervalOffset = 0;
+            //chartName.ChartAreas["ChartArea1"].AxisX.MinorGrid.IntervalOffsetType = DateTimeIntervalType.Auto;
+
+        }
+        public void YAxis(Chart chartName, string legendText, string labelMajorFamilyName, float labelMajorEmSize, FontStyle labelMajorStyle, int labelDecimals, double min, double max, double majorInt, bool minorEnabled, double minorIntFrac) // min and max need to be divisible by 1, 2 or 5
+        { // min and max need to be divisible by 1, 2 or 5
+            YAxisMin = min;
+            YAxisMax = max;
+            // Getting rid of divided by 0 error for Y2 axis calculation
+            if (majorInt <= 0d)
+            {
+                YAxisMajorInt = 1;
+            }
+            else
+            {
+                YAxisMajorInt = majorInt;
+            }
+
+            chartName.Series["Series1"].Enabled = true;
+
+            //chartName.Series["Series1"].ToolTip = "Series tooltip";
+
+            //chartName.Series["Series1"].LabelToolTip = "Label tooltip?";
+
+            chartName.Series["Series1"].IsVisibleInLegend = true;
+            chartName.Series["Series1"].LegendText = legendText;
+            //chartName.Series["Series1"].LegendToolTip = "Legend tooltip";
+
+            chartName.Series["Series1"].MarkerStyle = MarkerStyle.Circle;
+            chartName.Series["Series1"].MarkerStep = 1;
+
+            chartName.Series["Series1"].MarkerSize = 3;
+            chartName.Series["Series1"].MarkerColor = Color.Blue;
+            chartName.Series["Series1"].MarkerBorderWidth = 0;
+            chartName.Series["Series1"].MarkerBorderColor = Color.Blue;
+
+            chartName.ChartAreas["ChartArea1"].AxisY.Minimum = min;
+            chartName.ChartAreas["ChartArea1"].AxisY.Maximum = max;
+            chartName.ChartAreas["ChartArea1"].AxisY.Interval = majorInt;
+
+            chartName.ChartAreas["ChartArea1"].AxisY.MinorGrid.Enabled = minorEnabled;
+            chartName.ChartAreas["ChartArea1"].AxisY.MinorGrid.Interval = majorInt / minorIntFrac;
+
+            chartName.ChartAreas["ChartArea1"].AxisY.MinorGrid.LineColor = Color.DarkGray;
+            chartName.ChartAreas["ChartArea1"].AxisY.MinorGrid.LineDashStyle = ChartDashStyle.Dash;
+
+            chartName.ChartAreas["ChartArea1"].AxisY.LabelStyle.Font = new Font(labelMajorFamilyName, labelMajorEmSize, labelMajorStyle);
+            chartName.ChartAreas["ChartArea1"].AxisY.LabelStyle.Format = "F" + Convert.ToString(labelDecimals);
+            chartName.ChartAreas["ChartArea1"].AxisY.LabelStyle.ForeColor = Color.Blue;
+        }
+        public void Y2Axis(Chart chartName, string legendText, string labelMajorFamilyName, float labelMajorEmSize, FontStyle labelMajorStyle, int labelDecimals, double min, double max)// Others are used from the main YAxis
+        {
+            int numMajorGridLines = Convert.ToInt32((YAxisMax - YAxisMin) / YAxisMajorInt);
+            //Console.WriteLine(numMajorGridLines);
+            chartName.Series.Add("Series2");
+            chartName.Series["Series2"].LegendText = legendText;
+            chartName.Series["Series2"].ChartType = SeriesChartType.Point;
+
+            chartName.Series["Series2"].MarkerStyle = MarkerStyle.Circle;
+            chartName.Series["Series2"].MarkerStep = 1;
+
+            chartName.Series["Series2"].MarkerSize = 3;
+            chartName.Series["Series2"].MarkerColor = Color.Orange;
+            chartName.Series["Series2"].MarkerBorderWidth = 0;
+            chartName.Series["Series2"].MarkerBorderColor = Color.Orange;
+
+            chartName.Series["Series2"].YAxisType = AxisType.Secondary;
+
+            chartName.ChartAreas["ChartArea1"].AxisY2.LabelStyle.Font = new Font(labelMajorFamilyName, labelMajorEmSize, labelMajorStyle);
+
+            chartName.ChartAreas["ChartArea1"].AxisY2.LabelStyle.Format = "F" + Convert.ToString(labelDecimals);
+            chartName.ChartAreas["ChartArea1"].AxisY2.LabelStyle.ForeColor = Color.Orange;
+
+            chartName.ChartAreas["ChartArea1"].AxisY2.Minimum = min;
+            chartName.ChartAreas["ChartArea1"].AxisY2.Maximum = max;
+
+            //**********************************************************************************
+            // NOTE: Make these values divisible by 1, 2 or 5
+            chartName.ChartAreas["ChartArea1"].AxisY2.Interval = (max - min) / numMajorGridLines;
+            //**********************************************************************************
+        }
+
+        public void AutoScaleX(Chart chart)
+        {
+            chart.ChartAreas["ChartArea1"].AxisX.Minimum = double.NaN;
+            chart.ChartAreas["ChartArea1"].AxisX.Maximum = double.NaN;
+            chart.ChartAreas["ChartArea1"].AxisX.MajorGrid.Interval = double.NaN;
+            chart.ChartAreas["ChartArea1"].AxisX.Interval = 0;
+        }
+        public void AutoScaleY(Chart chart)
+        {
+            chart.ChartAreas["ChartArea1"].AxisY.Minimum = double.NaN;
+            chart.ChartAreas["ChartArea1"].AxisY.Maximum = double.NaN;
+            chart.ChartAreas["ChartArea1"].AxisY.MajorGrid.Interval = double.NaN;
+            chart.ChartAreas["ChartArea1"].AxisY.Interval = 0;
+        }
+        public void AutoScaleY2(Chart chart)
+        {
+            chart.ChartAreas["ChartArea1"].AxisY2.Minimum = double.NaN;
+            chart.ChartAreas["ChartArea1"].AxisY2.Maximum = double.NaN;
+        }
+
+        #endregion
+    }
+}
